37f05de297b532941c52fc4b5a0f5e5aa245221b
umaisyaqoob <umaisyaqoob333@gmail.com>
2025-01-15 00:25:35 +0500

    setup .env || settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Setup environment variables from .env file
+SECRET_KEY = os.getenv('SECRET_KEY')
